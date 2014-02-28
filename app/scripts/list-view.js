87fba139e3824737e0725f5123bbe7456f48fefa
Elizabeth Barr <elizbarr@gmail.com>
2014-02-28 12:02:18 -0500

    work on index.html

--- app/scripts/list-view.js
+++ app/scripts/list-view.js
@@ -1,4 +1,5 @@
 var Item = Backbone.Model.extend({});
+
 var ItemsCollection = Backbone.Collection.extend({
   model: Item,
   url: "https://api.gilt.com/v1/sales/active.json?apikey=24531d757413e8ccfd99762705d68ebf&includes=Images&callback=?"
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,7 +1,7 @@
 var MainRouter = Backbone.Router.extend({
   routes: {
-    "shops" : "showShops",
-    "shops/:keyword" : "showShops"
+    "items" : "showItems",
+    "items/:keyword" : "showItems"
   },
  
   initialize: function(){
@@ -11,7 +11,7 @@ var MainRouter = Backbone.Router.extend({
     })
   },
  
-  showShops: function(keyword){
+  showItems: function(keyword){
     if (keyword) {
       this.items.url = "https://api.gilt.com/v1/sales/upcoming.json?keywords="+ keyword +"&includes=Images&api_key=24531d757413e8ccfd99762705d68ebf&callback=?"
     }
